0de6d095e85e35306d6dec6d89007ed5289543d0
James Xu <jamesxuhaozhe@gmail.com>
2016-01-20 23:55:49 +0800

    create project repo

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "Learn-Scala-Thru-TDD"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  "junit" % "junit" % "4.8.1" % "test"
+)
+
---
+++ src/main/scala/helloworld/ScalaHelloWorld.scala
@@ -0,0 +1,12 @@
+package helloworld
+
+/**
+  * Created by haozhexu on 1/20/16.
+  */
+object ScalaHelloWorld {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+
+}
